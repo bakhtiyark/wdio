12fca20b829ccb614e0748e5d3b9577816da2276
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-05-13 15:25:33 +0500

    Variables moved to constants.json

--- tests/specs/test.spec.js
+++ tests/specs/test.spec.js
@@ -1,53 +1,49 @@
 ﻿// imports
 const { By, Builder, Select, until } = require("selenium-webdriver");
 require("chromedriver");
-
-// Variables
-
-const website = "https://pastebin.com";
-const textSpaceId = "postform-text";
-const message1 = "Hello from WebDriver";
-const message2 = `git config --global user.name "New Sheriff in Town"
-git reset $ (git commit-tree HEAD ^ {tree} -m "Legacy code")
-git push origin master --force`;
-const postformTitleId = "postform-name";
-const postForm1 = "helloweb";
-const postForm2 = "how to gain dominance among developers";
-const syntaxOptionContainerId = "select2-postform-format-container";
-const syntaxBashOption = "//li[text()='Bash']";
-const expirationContainer = "select2-postform-expiration-container";
-const expirationOption = "//li[text()='10 Minutes']";
-const submitBtnClassName = "btn -big";
+const constants = require("../../constants/constants.json");
 
 // #1
 const task1 = async () => {
   let driver = await new Builder().forBrowser("chrome").build();
 
-  driver.get(website);
+  driver.get(constants.website);
 
-  await driver.findElement(By.id(textSpaceId)).sendKeys(message1);
-  await driver.findElement(By.id(expirationContainer)).click();
-  await driver.findElement(By.xpath(expirationOption)).click();
-  await driver.findElement(By.id(postformTitleId)).sendKeys(postForm1);
+  await driver
+    .findElement(By.id(constants.textSpaceId))
+    .sendKeys(constants.message1);
+  await driver.findElement(By.id(constants.expirationContainer)).click();
+  await driver.findElement(By.xpath(constants.expirationOption)).click();
+  await driver
+    .findElement(By.id(constants.postformTitleId))
+    .sendKeys(constants.postForm1);
 
-  await driver.findElement(By.className(submitBtnClassName)).click();
+  await driver.findElement(By.className(constants.submitBtnClassName)).click();
 };
 // #2
 const task2 = async () => {
   let driver = await new Builder().forBrowser("chrome").build();
 
-  driver.get(website);
+  driver.get(constants.website);
 
-  await driver.findElement(By.id(textSpaceId)).sendKeys(message2);
-  await driver.findElement(By.id(syntaxOptionContainerId)).click();
-  await driver.findElement(By.xpath(syntaxBashOption)).click();
-  await driver.findElement(By.id(expirationContainer)).click();
-  await driver.findElement(By.xpath(expirationOption)).click();
-  await driver.findElement(By.id(postformTitleId)).sendKeys(postForm2);
-  await driver.findElement(By.className(submitBtnClassName)).click();
+  await driver
+    .findElement(By.id(constants.textSpaceId))
+    .sendKeys(constants.message2);
+  await driver.findElement(By.id(constants.syntaxOptionContainerId)).click();
+  await driver.findElement(By.xpath(constants.syntaxBashOption)).click();
+  await driver.findElement(By.id(constants.expirationContainer)).click();
+  await driver.findElement(By.xpath(constants.expirationOption)).click();
+  await driver
+    .findElement(By.id(constants.postformTitleId))
+    .sendKeys(constants.postForm2);
+  await driver.findElement(By.className(constants.submitBtnClassName)).click();
 
-  await driver.wait(until.titleIs(`${postForm2} - Pastebin.com`),10000,"Something went wrong");
+  await driver.wait(
+    until.titleIs(`${postForm2} - Pastebin.com`),
+    10000,
+    "Something went wrong"
+  );
 };
 
-//task1();
-//task2();
+task1();
+task2();
