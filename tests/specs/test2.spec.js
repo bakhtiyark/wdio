32caf4515a59f2a9648528252cc14c755cdaa0d5
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-05-14 19:05:26 +0500

    Minor

---
+++ tests/specs/test2.spec.js
@@ -0,0 +1,18 @@
+﻿// imports
+const { By, Builder, Select, until, Key } = require("selenium-webdriver");
+require("chromedriver");
+const constants = require("../../constants/compute.json");
+
+// #1
+const smoke = async () => {
+  let driver = await new Builder().forBrowser("chrome").build();
+
+  driver.get(constants.website);
+
+  await driver
+    .findElement(By.name("q"))
+    .sendKeys(constants.searchquery, Key.RETURN);
+  ;
+};
+
+smoke();
