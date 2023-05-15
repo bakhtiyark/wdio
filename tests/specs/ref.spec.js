97c45ee58ea8087665d43e927dda75a6bc3ee0c6
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-05-15 19:17:50 +0500

    xpath fix

---
+++ tests/specs/ref.spec.js
@@ -0,0 +1,8 @@
+﻿// imports
+const { By, Builder, Select, until, Key } = require("selenium-webdriver");
+require("chromedriver");
+const constants = require("../../constants/compute.json");
+
+describe("Hurt me plenty", function () {
+  it("GC", function () {});
+});
--- tests/specs/test2.spec.js
+++ tests/specs/test2.spec.js
@@ -8,11 +8,15 @@ const smoke = async () => {
   let driver = await new Builder().forBrowser("chrome").build();
 
   driver.get(constants.website);
-
   await driver
     .findElement(By.name("q"))
     .sendKeys(constants.searchquery, Key.RETURN);
-  ;
+  let ele = await driver.wait(
+    until.elementLocated(
+      By.xpath(`//b[contains(text(), "Google Cloud Platform ")]/parent::a`)
+    ),
+    10000, "Element not Found"
+  );
+  await ele.click()
 };
-
 smoke();
